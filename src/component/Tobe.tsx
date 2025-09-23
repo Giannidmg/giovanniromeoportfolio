50b9a7a9c10a52d615f50a3b7b0e5527fc2ad92e
Giannidmg <givanniromeo@gmail.com>
2025-09-23 12:54:05 +0200

    fix cname and path img

--- src/component/Tobe.tsx
+++ src/component/Tobe.tsx
@@ -1,7 +1,7 @@
 export function Tobe() {
   return (
   <div id="tobe">
-    <img id="tobelogo" src="public/logo.png" alt="tobelogo"></img>
+    <img id="tobelogo" src="/logo.png" alt="tobelogo"></img>
     <h1>Stay Tuned!</h1>
     <h2>Under construction will arrive in a few days...</h2>
   </div>
